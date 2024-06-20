6ea801bc70f086de6e731f2b6521027ccc9e748d
Zaud Rehman <121602708+ZaudRehman@users.noreply.github.com>
2024-06-20 14:30:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify
 from app.utils.database import db
 from flask_mail import Mail
 from app.config.config import get_config
-from app.controllers.auth_controller import bp as auth_bp
+from app.controllers.auth_controller import auth_bp
 import os
 
 # Initialize the Flask application
