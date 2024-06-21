00637aeac37e21d097c4ed4ba7968119698577fe
Zaud-R <rehmanzaud@gmail.com>
2024-06-21 11:01:17 +0530

    HEAD

--- app/__init__.py
+++ app/__init__.py
@@ -2,20 +2,16 @@ from flask import Flask
 from flask_mail import Mail
 from app.utils.database import db
 from app.config.config import get_config, Config
-<<<<<<< HEAD
 
-=======
->>>>>>> 2043370045ff1446ac1a6ea69c554173c8edb6e0
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(get_config())
-<<<<<<< HEAD
+
 
     mail = Mail(app)
     db.init_app(app, db_name=Config.DB_NAME)
-=======
->>>>>>> 2043370045ff1446ac1a6ea69c554173c8edb6e0
+
 
     mail = Mail(app)
     db.init_app(app, db_name=Config.DB_NAME)
